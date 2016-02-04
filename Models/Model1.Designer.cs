9dc9b9f11ee846b487a974ec0c918941be895128
bcsf12a012 <bcsf12a012@pucit.edu.pk>
2016-02-04 10:02:00 +0500

    termpro

---
+++ Models/Model1.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\UmerShahzad\documents\visual studio 2012\Projects\termProj\termProj\Models\Model1.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
